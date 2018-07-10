6a7f10045830585d4fa8c0751708f14f76e2b5a6
Kelvin Floresta de Andrade <kelvinf.contato@gmail.com>
2018-07-09 22:13:48 -0300

    atualização

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 express()
   .use(express.static(path.join(__dirname, "build")))
   .get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname,"index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   })
   .use(errorHandler)
   .listen(PORT, () => console.log("Listening on port =>", PORT));
